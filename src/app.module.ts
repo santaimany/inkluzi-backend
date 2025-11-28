af4b0ca301e6e3848c7ff10072e11674e0d9b9ce
santaimany <santaimany@gmail.com>
2025-11-28 20:39:50 +0700

    feat: add Email module with controller and service for email management

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthController } from './auth/auth.controller';
 import { ProfileModule } from './profile/profile.module';
 import { AdminModule } from './admin/admin.module';
+import { EmailModule } from './email/email.module';
 
 
 @Module({
-  imports: [AuthModule, PrismaModule, ProfileModule, AdminModule],
+  imports: [AuthModule, PrismaModule, ProfileModule, AdminModule, EmailModule],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
