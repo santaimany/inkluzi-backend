91d8282bec8cf5986171ea252b9373bba91ecfe4
santaimany <santaimany@gmail.com>
2025-12-04 23:58:26 +0700

    refactor: remove dotenv configuration for non-production environments

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv/config');
-}
 import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
   schema: 'prisma/schema.prisma',
